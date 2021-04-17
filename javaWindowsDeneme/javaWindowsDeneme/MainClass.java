8fab4cd0afd91ecd12f625198a6395918fb7331a
gokay <gokay@DESKTOP-6B9PKE2>
2021-04-17 14:52:41 +0300

    second...

--- javaWindowsDeneme/javaWindowsDeneme/MainClass.java
+++ javaWindowsDeneme/javaWindowsDeneme/MainClass.java
@@ -1,11 +1,13 @@
 package javaWindowsDeneme;
 
-public class mainClass {
+public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World !");
 		System.out.println("Hello Windows");
+		System.out.println("Hello kodlama.io");
 
 	}
 
